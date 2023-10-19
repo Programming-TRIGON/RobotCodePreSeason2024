bd1799a781115a1c29cb9ef538b42c2be2acb14b
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2023-10-19 22:27:55 +0300

    fixed small line mistakes

--- src/main/java/frc/trigon/robot/subsystems/collector/CollectorConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/collector/CollectorConstants.java
@@ -2,27 +2,25 @@ package frc.trigon.robot.subsystems.collector;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
-import com.ctre.phoenix6.signals.InvertedValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-
 public class CollectorConstants {
 
     private static final int MOTOR_ID = 1;
     private static final NeutralMode NEUTRAL_MODE_VALUE = NeutralMode.Brake;
-    private static final boolean inverted = true;
+    private static final boolean INVERTED = true;
     static final TalonSRX MOTOR = new TalonSRX(MOTOR_ID);
+
     static {
         MOTOR.setNeutralMode(NEUTRAL_MODE_VALUE);
-        MOTOR.setInverted(inverted);
+        MOTOR.setInverted(INVERTED);
     }
 
     public enum CollectorStates{
         COLLECT(-1),
         EJECT(1),
-        HOLD(-2);
+        HOLD(-0.2);
 
         double power;
+
         CollectorStates(double power){
             this.power = power;
         }
