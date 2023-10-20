7deffc0daca3d2acfeecd8e401fa7ebf4cf426eb
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2023-10-20 17:11:30 +0300

    added voltage compensation

--- src/main/java/frc/trigon/robot/subsystems/collector/CollectorConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/collector/CollectorConstants.java
@@ -7,9 +7,13 @@ public class CollectorConstants {
     private static final int MOTOR_ID = 1;
     private static final NeutralMode NEUTRAL_MODE_VALUE = NeutralMode.Brake;
     private static final boolean INVERTED = true;
+    private static final int VOLTAGE_COMP_SATURATION = 11;
+    private static final boolean ENABLE_VOLTAGE_COMPENSATION = true;
     static final TalonSRX MOTOR = new TalonSRX(MOTOR_ID);
 
     static {
+        MOTOR.configVoltageCompSaturation(VOLTAGE_COMP_SATURATION);
+        MOTOR.enableVoltageCompensation(ENABLE_VOLTAGE_COMPENSATION);
         MOTOR.configFactoryDefault();
         MOTOR.setNeutralMode(NEUTRAL_MODE_VALUE);
         MOTOR.setInverted(INVERTED);
