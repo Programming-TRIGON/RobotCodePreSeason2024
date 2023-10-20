2e36d03541e7c3c8c3aeea19baf0fa398f33caa3
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2023-10-20 17:19:37 +0300

    removed extra new line in subsystem

--- src/main/java/frc/trigon/robot/subsystems/collector/CollectorSubsystem.java
+++ src/main/java/frc/trigon/robot/subsystems/collector/CollectorSubsystem.java
@@ -2,7 +2,6 @@ package frc.trigon.robot.subsystems.collector;
 
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 public class CollectorSubsystem extends SubsystemBase {
 
     private final static CollectorSubsystem INSTANCE = new CollectorSubsystem();
