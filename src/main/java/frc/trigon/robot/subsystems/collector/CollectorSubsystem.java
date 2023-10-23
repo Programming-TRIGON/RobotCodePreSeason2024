4feaa598b0baa0e13db2db412d8ebe475eff22f4
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2023-10-23 18:56:09 +0300

    added note

--- src/main/java/frc/trigon/robot/subsystems/collector/CollectorSubsystem.java
+++ src/main/java/frc/trigon/robot/subsystems/collector/CollectorSubsystem.java
@@ -1,11 +1,12 @@
 package frc.trigon.robot.subsystems.collector;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class CollectorSubsystem extends SubsystemBase {
     private final static CollectorSubsystem INSTANCE = new CollectorSubsystem();
+    private final TalonSRX motor = CollectorConstants.MOTOR;
+    // motor.getSupplyCurrent
 
     public static CollectorSubsystem getInstance() {
         return INSTANCE;
