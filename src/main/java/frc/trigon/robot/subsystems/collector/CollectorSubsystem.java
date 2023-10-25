f0cb6432357da33294a38059b3b0f818ad1622f8
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2023-10-25 15:30:34 +0300

    removed extra new lines

--- src/main/java/frc/trigon/robot/subsystems/collector/CollectorSubsystem.java
+++ src/main/java/frc/trigon/robot/subsystems/collector/CollectorSubsystem.java
@@ -7,8 +7,6 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import java.util.function.Supplier;
-
 public class CollectorSubsystem extends SubsystemBase {
     private final static CollectorSubsystem INSTANCE = new CollectorSubsystem();
     private final TalonSRX motor = CollectorConstants.MOTOR;
@@ -17,9 +15,7 @@ public class CollectorSubsystem extends SubsystemBase {
     public static CollectorSubsystem getInstance() {
         return INSTANCE;
     }
-
-
-
+    
     private CollectorSubsystem() {
     }
 
