169b9e7835866bb423bf9c3aa627eb610a1f7b3e
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2023-10-30 21:01:55 +0200

    attempted the current watcher
    I've tried making the time part of the current watcher, I'm not sure how I'm gonna do the rest. and I don't know how to use the runnable

--- src/main/java/frc/trigon/robot/utilities/CurrentWatcher.java
+++ src/main/java/frc/trigon/robot/utilities/CurrentWatcher.java
@@ -1,6 +1,22 @@
 package frc.trigon.robot.utilities;
 
-public class CurrentWatcher {
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.NotifierCommand;
 
-    
+import java.util.function.Supplier;
+
+public class CurrentWatcher {
+    double prevTime;
+
+    public void currentWatcher(Runnable runnable, Supplier<Integer> current, Supplier<Integer> maxAmount, Supplier<Double> time){
+        if (current.get() < maxAmount.get()){
+            this.prevTime = time.get();
+            return;
+        }
+        double currentTime = time.get();
+        double difference = currentTime - prevTime;
+        
+    }
 }
