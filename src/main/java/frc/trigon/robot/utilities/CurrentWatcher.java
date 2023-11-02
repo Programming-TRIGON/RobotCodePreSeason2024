7f0bbc8bb0908ff799177fdcfc31ddf91ba11dd5
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2023-11-02 16:59:53 +0200

    made CurrentWatcher

--- src/main/java/frc/trigon/robot/utilities/CurrentWatcher.java
+++ src/main/java/frc/trigon/robot/utilities/CurrentWatcher.java
@@ -1,24 +1,54 @@
 package frc.trigon.robot.utilities;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.Notifier;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.NotifierCommand;
+import edu.wpi.first.wpilibj.Timer;
 
 import java.util.function.Supplier;
 
+/**
+ * A class that checks if the current past a certain limit for a certain amount of time.
+ */
 public class CurrentWatcher {
-    private double previousTime;
+    private double bellowCurrentTime;
+    private final Runnable runnable;
+    private final Supplier<Integer> currentSupplier;
+    private final int maxCurrent;
+    private final double timeThreshold;
 
-    public void currentWatcher(Runnable runnable, Supplier<Integer> currentSupplier, int maxCurrent, double timeThreshold){
-        Notifier notifier = new Notifier(runnable);
-        notifier.startPeriodic(0.02);
-        if (currentSupplier.get() < maxCurrent){
-            this.previousTime = timeThreshold;
+    /**
+     * Checks the current every 0.02 seconds, and it passes runs a runnable.
+     *
+     * @param runnable        what will run if the current passes it's limit
+     * @param currentSupplier the current
+     * @param maxCurrent      the current limit
+     * @param timeThreshold   the time
+     */
+    public CurrentWatcher(Runnable runnable, Supplier<Integer> currentSupplier, int maxCurrent, double timeThreshold) {
+        this.runnable = runnable;
+        this.currentSupplier = currentSupplier;
+        this.maxCurrent = maxCurrent;
+        this.timeThreshold = timeThreshold;
+
+        new Notifier(this::checkCurrent).startPeriodic(0.02);
+    }
+
+    private void checkCurrent() {
+        if (isBellowCurrentLimit()) {
+            bellowCurrentTime = Timer.getFPGATimestamp();
             return;
         }
-        double currentTime = timeThreshold;
-        double difference = currentTime - previousTime;
-
+        if (didPassTimeThreshold())
+            runnable.run();
     }
+
+    private boolean isBellowCurrentLimit() {
+        return currentSupplier.get() <= maxCurrent;
+    }
+
+    private boolean didPassTimeThreshold() {
+        double timeDifference = Timer.getFPGATimestamp() - bellowCurrentTime;
+        return timeThreshold > timeDifference;
+    }
+
+
 }
