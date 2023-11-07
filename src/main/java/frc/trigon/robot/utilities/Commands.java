770f2c61d3f0b3c054270195a8d6933936dded34
Strflightmight09 <148347057+Strflightmight09@users.noreply.github.com>
2023-11-07 15:24:51 +0200

    Roller (#2)
    * Created constants and most of roller subsystem
    
    * Finish constants and commands
    
    * Fixes made to comments
    
    * Added commands to remove requirements. Fixed Javadoc
    
    * Fixed formatting issues
    
    * Fixed requested changes
    
    * Fixed requested changes
    
    * Fixed requested changes
    
    * Changed "commandBase" to "command"

---
+++ src/main/java/frc/trigon/robot/utilities/Commands.java
@@ -0,0 +1,15 @@
+package frc.trigon.robot.utilities;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+
+public class Commands {
+    public static Command removeRequirements(CommandBase command) {
+        return new FunctionalCommand(
+                command::initialize,
+                command::execute,
+                command::end,
+                command::isFinished
+        );
+    }
+}
