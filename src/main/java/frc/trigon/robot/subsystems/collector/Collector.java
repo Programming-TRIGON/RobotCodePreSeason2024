18f9f470d37271006d101509ce02265083d35dff
Nummun14 <nahum.elbaum@gmail.com>
2023-11-07 18:28:37 +0200

    made startCurrentWatcher

--- src/main/java/frc/trigon/robot/subsystems/collector/Collector.java
+++ src/main/java/frc/trigon/robot/subsystems/collector/Collector.java
@@ -7,8 +7,8 @@ import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.trigon.robot.utilities.CurrentWatcher;
 
-public class CollectorSubsystem extends SubsystemBase {
-    private final static CollectorSubsystem INSTANCE = new CollectorSubsystem();
+public class Collector extends SubsystemBase {
+    private final static Collector INSTANCE = new Collector();
     private final TalonSRX motor = CollectorConstants.MOTOR;
     private final CurrentWatcher currentWatcher = new CurrentWatcher(
             this::stop,
@@ -17,11 +17,12 @@ public class CollectorSubsystem extends SubsystemBase {
             CollectorConstants.MAX_CURRENT_TIME
     );
 
-    public static CollectorSubsystem getInstance() {
+    public static Collector getInstance() {
         return INSTANCE;
     }
 
-    private CollectorSubsystem() {
+    private Collector() {
+        startCurrentWatcher();
     }
 
     public Command getSetTargetStateCommand(CollectorConstants.CollectorStates state) {
@@ -34,5 +35,14 @@ public class CollectorSubsystem extends SubsystemBase {
 
     private void stop() {
         motor.set(ControlMode.Disabled, 0);
+    }
+
+    private void startCurrentWatcher(){
+        new CurrentWatcher(
+                this::stop,
+                motor::getSupplyCurrent,
+                CollectorConstants.MAX_CURRENT,
+                CollectorConstants.MAX_CURRENT_TIME
+        );
     }
 }
