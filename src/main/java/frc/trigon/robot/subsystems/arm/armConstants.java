36c5c3c58a7dbb82f9f9efe31922a4ca55df6863
Nummun14 <nahum.elbaum@gmail.com>
2023-11-08 20:39:40 +0200

    added PIDControler

--- src/main/java/frc/trigon/robot/subsystems/arm/armConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/arm/armConstants.java
@@ -2,6 +2,7 @@ package frc.trigon.robot.subsystems.arm;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
+import edu.wpi.first.math.controller.PIDController;
 
 public class armConstants {
 
@@ -14,6 +15,13 @@ public class armConstants {
     private static final CANSparkMax
             CAN_SPARK_ANGLE = new CANSparkMax(ANGLE_CAN_SPARK_ID, CANSparkMaxLowLevel.MotorType.kBrushless),
             CAN_SPARK_ELEVATOR = new CANSparkMax(ELEVATOR_CAN_SPARK_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
+
+    static final double
+            P = 1,
+            I = 0,
+            D = 0;
+
+    static final PIDController PID_CONTROLLER = new PIDController(P, I, D);
 
     static {
         CAN_SPARK_ANGLE.restoreFactoryDefaults();
