6546c5479173e1303079f16d2ad5294d996056a0
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2023-11-09 09:49:28 +0200

    fixed angle encoder config

--- src/main/java/frc/trigon/robot/subsystems/arm/ArmConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/arm/ArmConstants.java
@@ -16,7 +16,7 @@ public class ArmConstants {
             ANGLE_NEO_ID = 3,
             ELEVATOR_NEO_ID = 4,
             TALON_SRX_ID = 5,
-            CAN_CODER_ID = 6;
+            ANGLE_ENCODER_ID = 6;
 
     private static final CANSparkMax
             ANGLE_MOTOR = new CANSparkMax(ANGLE_MOTOR_ID, CANSparkMaxLowLevel.MotorType.kBrushless),
@@ -25,7 +25,7 @@ public class ArmConstants {
             ELEVATOR_IDLE_MODE = CANSparkMax.IdleMode.kCoast,
             ANGLE_IDLE_MODE = CANSparkMax.IdleMode.kCoast;
     private static final TalonSRX TALON_SRX = new TalonSRX(TALON_SRX_ID);
-    private static final CANCoder CAN_CODER = new CANCoder(CAN_CODER_ID);
+    private static final CANCoder ANGLE_ENCODER = new CANCoder(ANGLE_ENCODER_ID);
 
     private static final NeutralMode TALON_SRX_NEUTRAL_MODE_VALUE = NeutralMode.Coast;
     private static final boolean TALON_SRX_INVERTED = false;
@@ -37,9 +37,9 @@ public class ArmConstants {
     private static final boolean
             ELEVATOR_INVERTED = false,
             ANGLE_INVERTED = false,
-            CAN_CODER_DIRECTION = false;
-    private static final AbsoluteSensorRange CAN_CODER_RANGE = new AbsoluteSensorRange(0.5, -0.5);
-    private static final double CAN_CODER_OFFSET = 10;
+            ANGLE_ENCODER_DIRECTION = false;
+    private static final AbsoluteSensorRange ANGLE_ENCODER_RANGE = new AbsoluteSensorRange(0.5, -0.5);
+    private static final double ANGLE_ENCODER_OFFSET = 10;
 
     static final double
             P = 1,
@@ -49,14 +49,8 @@ public class ArmConstants {
     static final PIDController PID_CONTROLLER = new PIDController(P, I, D);
 
     static {
-        ANGLE_MOTOR.restoreFactoryDefaults();
-        ELEVATOR_MOTOR.restoreFactoryDefaults();
-        ANGLE_MOTOR.setIdleMode(ANGLE_IDLE_MODE);
-        ELEVATOR_MOTOR.setIdleMode(ELEVATOR_IDLE_MODE);
-        ANGLE_MOTOR.setInverted(ANGLE_INVERTED);
-        ELEVATOR_MOTOR.setInverted(ELEVATOR_INVERTED);
-        ANGLE_MOTOR.enableVoltageCompensation(ANGLE_MOTOR_VOLTAGE_COMPENSATION_SATURATION);
-        ELEVATOR_MOTOR.enableVoltageCompensation(ELEVATOR_MOTOR_VOLTAGE_COMPENSATION_SATURATION);
+        configureAngleMotor();
+        configureElevatorMotor();
 
         TALON_SRX.configFactoryDefault();
         TALON_SRX.configVoltageCompSaturation(TALON_SRX_VOLTAGE_COMPENSATION_SATURATION);
@@ -64,16 +58,34 @@ public class ArmConstants {
         TALON_SRX.setNeutralMode(TALON_SRX_NEUTRAL_MODE_VALUE);
         TALON_SRX.setInverted(TALON_SRX_INVERTED);
 
-        CAN_CODER.configFactoryDefault();
-        CANCoderConfiguration canCoderConfig = new CANCoderConfiguration();
-        canCoderConfig.sensorDirection = CAN_CODER_DIRECTION;
-        canCoderConfig.absoluteSensorRange = CAN_CODER_RANGE;
-        canCoderConfig.magnetOffsetDegrees = CAN_CODER_OFFSET;
-        CAN_CODER.configAllSettings(canCoderConfig);
+        configureAngleEncoder();
     }
 
     public enum ArmState{
         ANGLE,
         ELEVATOR_POSITION
+    }
+
+    private static void configureElevatorMotor(){
+        ELEVATOR_MOTOR.restoreFactoryDefaults();
+        ELEVATOR_MOTOR.setIdleMode(ELEVATOR_IDLE_MODE);
+        ELEVATOR_MOTOR.setInverted(ELEVATOR_INVERTED);
+        ELEVATOR_MOTOR.enableVoltageCompensation(ELEVATOR_MOTOR_VOLTAGE_COMPENSATION_SATURATION);
+    }
+
+    private static void configureAngleMotor(){
+        ANGLE_MOTOR.restoreFactoryDefaults();
+        ANGLE_MOTOR.setIdleMode(ANGLE_IDLE_MODE);
+        ANGLE_MOTOR.setInverted(ANGLE_INVERTED);
+        ANGLE_MOTOR.enableVoltageCompensation(ANGLE_MOTOR_VOLTAGE_COMPENSATION_SATURATION);
+    }
+
+    private static void configureAngleEncoder(){
+        ANGLE_ENCODER.configFactoryDefault();
+        CANCoderConfiguration angleEncoderConfig = new CANCoderConfiguration();
+        angleEncoderConfig.sensorDirection = ANGLE_ENCODER_DIRECTION;
+        angleEncoderConfig.absoluteSensorRange = ANGLE_ENCODER_RANGE;
+        angleEncoderConfig.magnetOffsetDegrees = ANGLE_ENCODER_OFFSET;
+        ANGLE_ENCODER.configAllSettings(angleEncoderConfig);
     }
 }
