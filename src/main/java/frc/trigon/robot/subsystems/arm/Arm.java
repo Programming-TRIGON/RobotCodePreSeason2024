17bbef8ac555234c1e63d7baa70d8ccc881136bc
Strflightmight09 <ezragryn57@gmail.com>
2023-11-12 13:53:58 +0200

    Completed requested changes

--- src/main/java/frc/trigon/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/trigon/robot/subsystems/arm/Arm.java
@@ -5,10 +5,6 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase {
 
-    // With eager singleton initialization, any static variables/fields used in the 
-    // constructor must appear before the "INSTANCE" variable so that they are initialized 
-    // before the constructor is called when the "INSTANCE" variable initializes.
-
     private final static Arm INSTANCE = new Arm();
 
     public static Arm getInstance() {
--- src/main/java/frc/trigon/robot/subsystems/arm/ArmConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/arm/ArmConstants.java
@@ -11,48 +11,22 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public class ArmConstants {
-    private static final int
-            MASTER_ANGLE_MOTOR_ID = 0,
-            FOLLOWER_ANGLE_MOTOR_ID = 1,
-            MASTER_ELEVATOR_MOTOR_ID = 0,
-            FOLLOWER_ELEVATOR_MOTOR_ID = 1,
-            ANGLE_ENCODER_ID = 0,
-            ELEVATOR_ENCODER_ID = 1;
-    private static final boolean
-            MASTER_ANGLE_MOTOR_INVERTED = false,
-            FOLLOWER_ANGLE_MOTOR_INVERTED = false,
-            MASTER_ELEVATOR_MOTOR_INVERTED = false,
-            FOLLOWER_ELEVATOR_MOTOR_INVERTED = false;
-    private static final CANSparkMax.IdleMode
-            ANGLE_MOTORS_IDLE_MODE = CANSparkMax.IdleMode.kBrake,
-            ELEVATOR_MOTORS_IDLE_MODE = CANSparkMax.IdleMode.kBrake;
+    private static final int MASTER_ANGLE_MOTOR_ID = 0, FOLLOWER_ANGLE_MOTOR_ID = 1, MASTER_ELEVATOR_MOTOR_ID = 0, FOLLOWER_ELEVATOR_MOTOR_ID = 1, ANGLE_ENCODER_ID = 0, ELEVATOR_ENCODER_ID = 1;
+    private static final boolean MASTER_ANGLE_MOTOR_INVERTED = false, FOLLOWER_ANGLE_MOTOR_INVERTED = false, MASTER_ELEVATOR_MOTOR_INVERTED = false, FOLLOWER_ELEVATOR_MOTOR_INVERTED = false;
+    private static final CANSparkMax.IdleMode ANGLE_MOTORS_IDLE_MODE = CANSparkMax.IdleMode.kBrake, ELEVATOR_MOTORS_IDLE_MODE = CANSparkMax.IdleMode.kBrake;
     private static final int VOLTAGE_COMPENSATION_SATURATION = 12;
-    private static final CANSparkMax MASTER_ANGLE_MOTOR = new CANSparkMax(MASTER_ANGLE_MOTOR_ID, CANSparkMaxLowLevel.MotorType.kBrushless),
-            FOLLOWER_ANGLE_MOTOR = new CANSparkMax(FOLLOWER_ANGLE_MOTOR_ID, CANSparkMaxLowLevel.MotorType.kBrushless),
-            MASTER_ELEVATOR_MOTOR = new CANSparkMax(MASTER_ELEVATOR_MOTOR_ID, CANSparkMaxLowLevel.MotorType.kBrushless),
-            FOLLOWER_ELEVATOR_MOTOR = new CANSparkMax(FOLLOWER_ELEVATOR_MOTOR_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
+    private static final CANSparkMax MASTER_ANGLE_MOTOR = new CANSparkMax(MASTER_ANGLE_MOTOR_ID, CANSparkMaxLowLevel.MotorType.kBrushless), FOLLOWER_ANGLE_MOTOR = new CANSparkMax(FOLLOWER_ANGLE_MOTOR_ID, CANSparkMaxLowLevel.MotorType.kBrushless), MASTER_ELEVATOR_MOTOR = new CANSparkMax(MASTER_ELEVATOR_MOTOR_ID, CANSparkMaxLowLevel.MotorType.kBrushless), FOLLOWER_ELEVATOR_MOTOR = new CANSparkMax(FOLLOWER_ELEVATOR_MOTOR_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
     private static final CANcoder ANGLE_ENCODER = new CANcoder(ANGLE_ENCODER_ID);
     private static final TalonFX ELEVATOR_ENCODER = new TalonFX(ELEVATOR_ENCODER_ID);
-
-
-    //                                               |
-    //I need help with this one                      V
-    private static final double ANGLE_MOTOR_OFFSET = 0;
+    private static final double ANGLE_MOTOR_OFFSET = 1;
     private static final SensorDirectionValue ANGLE_MOTOR_DIRECTION = SensorDirectionValue.Clockwise_Positive;
     private static final AbsoluteSensorRangeValue ANGLE_MOTOR_RANGE = AbsoluteSensorRangeValue.Signed_PlusMinusHalf;
-    private static final int
-            P = 0,
-            I = 0,
-            D = 0;
-    private static final PIDController PID_CONTROLLER = new PIDController(P, I, D);
+    private static final int P = 0, I = 0, D = 0;
+    private static final PIDController PID_CONTROLLER_ANGLE = new PIDController(P, I, D);
+    private static final PIDController PID_CONTROLLER_ELEVATOR = new PIDController(P, I, D);
 
     public enum ArmState {
-        DEFAULT(Rotation2d.fromDegrees(0), 0),
-        TAKE_HIGH_CONE(Rotation2d.fromDegrees(60), 10),
-        TAKE_GROUND_CONE(Rotation2d.fromDegrees(0), 0),
-        PLACE_LOW_CONE(Rotation2d.fromDegrees(20), 3),
-        PLACE_MEDIUM_CONE(Rotation2d.fromDegrees(50), 7),
-        PLACE_HIGH_CONE(Rotation2d.fromDegrees(70), 10);
+        DEFAULT(Rotation2d.fromDegrees(0), 0), TAKE_HIGH_CONE(Rotation2d.fromDegrees(60), 10), TAKE_GROUND_CONE(Rotation2d.fromDegrees(0), 0), PLACE_LOW_CONE(Rotation2d.fromDegrees(20), 3), PLACE_MEDIUM_CONE(Rotation2d.fromDegrees(50), 7), PLACE_HIGH_CONE(Rotation2d.fromDegrees(70), 10);
 
         final Rotation2d angle;
         final double elevatorPosition;
@@ -96,8 +70,8 @@ public class ArmConstants {
     private static void configureAngleEncoder() {
         CANcoderConfiguration config = new CANcoderConfiguration();
         config.MagnetSensor.MagnetOffset = ANGLE_MOTOR_OFFSET;
-        config.MagnetSensor.SensorDirection.value = ANGLE_MOTOR_DIRECTION;
-        config.MagnetSensor.AbsoluteSensorRange.value = ANGLE_MOTOR_RANGE;
+        config.MagnetSensor.SensorDirection = ANGLE_MOTOR_DIRECTION;
+        config.MagnetSensor.AbsoluteSensorRange = ANGLE_MOTOR_RANGE;
         ANGLE_ENCODER.getConfigurator().apply(config);
     }
 
