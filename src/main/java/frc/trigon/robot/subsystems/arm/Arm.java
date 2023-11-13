08ffebcfa9267cc67c0c902e094d04b41d3d3925
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2023-11-13 11:30:33 +0200

    made arm a subsystem

--- src/main/java/frc/trigon/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/trigon/robot/subsystems/arm/Arm.java
@@ -1,4 +1,19 @@
 package frc.trigon.robot.subsystems.arm;
 
-public class Arm {
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Arm extends SubsystemBase {
+
+    private final static Arm INSTANCE = new Arm();
+
+    public static Arm getInstance() {
+        return INSTANCE;
+    }
+
+    private Arm() {
+    }
+
+    
 }
+
