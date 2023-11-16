50fbce08c703bc28ce5b94b3b495b84eb476e78b
ofirKAMInetsky <ofir.kam09@gmail.com>
2023-11-16 09:12:32 +0200

    ____added___motors___

--- src/main/java/frc/trigon/robot/subsystems/arm/ArmSubsystem.java
+++ src/main/java/frc/trigon/robot/subsystems/arm/ArmSubsystem.java
@@ -1,11 +1,21 @@
 package frc.trigon.robot.subsystems.arm;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase {
+    static final CANSparkMax
+            masterAngleMotor = ArmConstants.MASTER_ANGLE_MOTOR,
+            followerAngleMotor = ArmConstants.FOLLOWER_ANGLE_MOTOR,
+            masterElevatorMotor = ArmConstants.MASTER_ELEVATOR_MOTOR,
+            followerElevatorMotor = ArmConstants.FOLLOWER_ELEVATOR_MOTOR;
+    static final TalonSRX elevatorEncoder = ArmConstants.ELEVATOR_ENCODER;
+    static final CANcoder angleEncoder = ArmConstants.ANGLE_ENCODER;
 
     private final static ArmSubsystem INSTANCE = new ArmSubsystem();
-
     @SuppressWarnings("WeakerAccess")
     public static ArmSubsystem getInstance() {
         return INSTANCE;
