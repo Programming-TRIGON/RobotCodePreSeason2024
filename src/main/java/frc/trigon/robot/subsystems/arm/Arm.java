1dcd1e40f4f55dedf3d4232d291185815bf2f3b0
ofirKAMInetsky <ofir.kam09@gmail.com>
2023-11-16 22:46:31 +0200

    added angle methods

--- src/main/java/frc/trigon/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/trigon/robot/subsystems/arm/Arm.java
@@ -5,6 +5,11 @@ import com.ctre.phoenix6.hardware.CANcoder;
 import com.revrobotics.CANSparkMax;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase {
@@ -22,6 +27,50 @@ public class Arm extends SubsystemBase {
 
     private final ArmFeedforward angleMotorFeedforward = frc.trigon.robot.subsystems.arm.ArmConstants.ANGLE_MOTOR_FEEDFORWARD;
     private final ElevatorFeedforward elevatorMotorFeedforward = frc.trigon.robot.subsystems.arm.ArmConstants.ELEVATOR_MOTOR_FEEDFORWARD;
+
+    private TrapezoidProfile
+            angleMotorProfile = null,
+            elevatorMotorProfile = null;
+    private double
+            lastAngleProfileGeneration,
+            lastElevatorProfileGeneration;
+    private double getAngleMotorPositionDegrees(){
+        return angleEncoder.getPosition().getValue();
+    }
+    private double getAngleMotorVelocity(){
+        return masterAngleMotor.getVoltageCompensationNominalVoltage();
+    }
+
+        private void generateAngleMotorProfile(Rotation2d targetAngle){
+        angleMotorProfile = new TrapezoidProfile(
+                ArmConstants.ANGLE_CONSTRAINS,
+                new TrapezoidProfile.State(targetAngle.getDegrees(),0),
+                new TrapezoidProfile.State(getAngleMotorPositionDegrees(), getAngleMotorVelocity())
+        );
+        lastAngleProfileGeneration = Timer.getFPGATimestamp();
+    }
+
+    private double getAngleMotorProfileTime(){
+        return Timer.getFPGATimestamp() - lastAngleProfileGeneration;
+    }
+
+    private void setTargetAngleFromProfile(){
+        if (angleMotorProfile == null){
+            masterAngleMotor.stopMotor();
+            followerAngleMotor.stopMotor();
+            return;
+        }
+        TrapezoidProfile.State targetState = angleMotorProfile.calculate(getAngleMotorProfileTime());
+        double pidOutPut = ArmConstants.ANGLE_PID_CONTROLLER.calculate(
+                getAngleMotorPositionDegrees(),
+                targetState.position
+        );
+        double feedForward = ArmConstants.ANGLE_MOTOR_FEEDFORWARD.calculate(
+                Units.degreesToRadians(targetState.position),
+                Units.degreesToRadians(targetState.velocity)
+        );
+        masterAngleMotor.setVoltage(pidOutPut + feedForward);
+    }
 
     private Arm() {
     }
