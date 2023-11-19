e029a41d2ab1e9574da9549cef31308a78324df4
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2023-11-19 19:43:25 +0200

    added new line

--- src/main/java/frc/trigon/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/trigon/robot/subsystems/arm/Arm.java
@@ -9,6 +9,7 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.*;
 import frc.trigon.robot.utilities.Conversions;
+
 import java.util.Set;
 
 public class Arm extends SubsystemBase {
