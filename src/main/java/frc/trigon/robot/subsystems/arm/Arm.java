14a9e9780e92485f05ccb17fb261d4665bf5c550
ofirKAMInetsky <ofir.kam09@gmail.com>
2023-11-19 20:16:14 +0200

    !@#$%^&*()_FIX_)(*&^%$#@!

--- src/main/java/frc/trigon/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/trigon/robot/subsystems/arm/Arm.java
@@ -7,6 +7,8 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.trigon.robot.utilities.Conversions;
 
@@ -32,6 +34,17 @@ public class Arm extends SubsystemBase {
     }
 
     private Arm() {
+    }
+
+    public Command getSetTargetArmAngleCommand(Rotation2d angle){
+        return new FunctionalCommand(
+                () -> generateAngleMotorProfile(angle),
+                this::setTargetAngleFromProfile,
+                (interrupted) -> {
+                },
+                () -> false,
+                this
+        );
     }
 
     private void generateAngleMotorProfile(Rotation2d targetAngle) {
