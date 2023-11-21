cbd408c4951eae16bd435c8d6d1bcf38a72df75a
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2023-11-21 17:37:58 +0200

    added updateInputs

--- src/main/java/frc/trigon/robot/subsystems/collector/kablamacollector/KablamaCollectorIO.java
+++ src/main/java/frc/trigon/robot/subsystems/collector/kablamacollector/KablamaCollectorIO.java
@@ -3,9 +3,16 @@ package frc.trigon.robot.subsystems.collector.kablamacollector;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import frc.trigon.robot.subsystems.collector.CollectorIO;
+import frc.trigon.robot.subsystems.collector.CollectorInputsAutoLogged;
 
 public class KablamaCollectorIO extends CollectorIO {
     private final TalonSRX motor = KablamaCollectorConstants.MOTOR;
+
+    @Override
+    protected void updateInputs(CollectorInputsAutoLogged inputs) {
+        inputs.motorVoltage = motor.getMotorOutputVoltage();
+        inputs.motorCurrent = motor.getSupplyCurrent();
+    }
 
     @Override
     protected void setPower(double power) {
