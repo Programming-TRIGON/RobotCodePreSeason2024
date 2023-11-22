930fe123bd8cb65abf538223fe2f4d27fb9ed30c
yisha23 <yishayamir@gmail.com>
2023-11-22 18:02:30 +0200

    fix according to the rewiew

--- src/main/java/frc/trigon/robot/subsystems/sideshooter/SideShooterConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/sideshooter/SideShooterConstants.java
@@ -11,44 +11,48 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 
 public class SideShooterConstants {
-    private static final int SHOOTING_MOTOR_ID = 0;
-    private static final int ANGEL_MOTOR_ID = 0;
-    private static final int ENCODER_ID = 0;
+    private static final int
+            SHOOTING_MOTOR_ID = 0,
+            ANGLE_MOTOR_ID = 0,
+            ANGLE_ENCODER_ID = 0;
     private static final NeutralModeValue SHOOTING_NEUTRAL_MODE_VALUE = NeutralModeValue.Coast;
-    private static final CANSparkMax.IdleMode ANGLE_NEUTRAL_MODE_VALUE = CANSparkMax.IdleMode.kBrake;
+    private static final CANSparkMax.IdleMode ANGLE_IDLE_MODE = CANSparkMax.IdleMode.kBrake;
     private static final InvertedValue SHOOTING_INVERTED_VALUE = InvertedValue.CounterClockwise_Positive;
-    private static final boolean ANGLE_INVERTED_VALUE = false;
+    private static final boolean ANGLE_INVERTED = false;
     private static final double ENCODER_OFFSET = 0;
     private static final AbsoluteSensorRangeValue ENCODER_RANGE_VALUE = AbsoluteSensorRangeValue.Signed_PlusMinusHalf;
-    static final CANcoder ENCODER = new CANcoder(ENCODER_ID);
+    private static final double VOLTAGE_COMPENSATION_VALUE = 0;
+    static final CANcoder ANGLE_ENCODER = new CANcoder(ANGLE_ENCODER_ID);
     static final TalonFX SHOOTING_MOTOR = new TalonFX(SHOOTING_MOTOR_ID);
-    static final CANSparkMax ANGEL_MOTOR = new CANSparkMax(ANGEL_MOTOR_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
+    static final CANSparkMax ANGEL_MOTOR = new CANSparkMax(ANGLE_MOTOR_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
 
     static {
         configureShootingMotor();
         configureAngleMotor();
-        configureEncoder();
+        configureAngleEncoder();
     }
+
     private static void configureShootingMotor() {
-        TalonFXConfiguration ShootingMotorConfig = new TalonFXConfiguration();
-        ShootingMotorConfig.Audio.BeepOnBoot = false;
-        ShootingMotorConfig.Audio.BeepOnConfig = false;
-        ShootingMotorConfig.MotorOutput.NeutralMode = SHOOTING_NEUTRAL_MODE_VALUE;
-        ShootingMotorConfig.MotorOutput.Inverted = SHOOTING_INVERTED_VALUE;
-        SHOOTING_MOTOR.getConfigurator().apply(ShootingMotorConfig);
+        TalonFXConfiguration config = new TalonFXConfiguration();
+        config.Audio.BeepOnBoot = false;
+        config.Audio.BeepOnConfig = false;
+        config.MotorOutput.NeutralMode = SHOOTING_NEUTRAL_MODE_VALUE;
+        config.MotorOutput.Inverted = SHOOTING_INVERTED_VALUE;
+        SHOOTING_MOTOR.getConfigurator().apply(config);
     }
 
     private static void configureAngleMotor(){
         ANGEL_MOTOR.restoreFactoryDefaults();
-        ANGEL_MOTOR.setIdleMode(ANGLE_NEUTRAL_MODE_VALUE);
-        ANGEL_MOTOR.setInverted(ANGLE_INVERTED_VALUE);
+        ANGEL_MOTOR.setIdleMode(ANGLE_IDLE_MODE);
+        ANGEL_MOTOR.setInverted(ANGLE_INVERTED);
+        ANGEL_MOTOR.enableVoltageCompensation(VOLTAGE_COMPENSATION_VALUE);
     }
 
-    private static void configureEncoder(){
-        CANcoderConfiguration EncoderConfiguration = new CANcoderConfiguration();
-        EncoderConfiguration.MagnetSensor.MagnetOffset = ENCODER_OFFSET;
-        EncoderConfiguration.MagnetSensor.AbsoluteSensorRange = ENCODER_RANGE_VALUE;
-        ENCODER.getConfigurator().apply(EncoderConfiguration);
+    private static void configureAngleEncoder(){
+        CANcoderConfiguration config = new CANcoderConfiguration();
+        config.MagnetSensor.MagnetOffset = ENCODER_OFFSET;
+        config.MagnetSensor.AbsoluteSensorRange = ENCODER_RANGE_VALUE;
+        ANGLE_ENCODER.getConfigurator().apply(config);
     }
 
 }
