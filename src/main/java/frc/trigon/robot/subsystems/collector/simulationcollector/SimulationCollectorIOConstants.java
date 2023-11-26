b51537d5e399fe5af4c98b08d4e8f636f02330f6
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2023-11-26 14:48:40 +0200

    attempted to add ElevatorSim and SingleJointedArmSim

--- src/main/java/frc/trigon/robot/subsystems/collector/simulationcollector/SimulationCollectorIOConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/collector/simulationcollector/SimulationCollectorIOConstants.java
@@ -2,13 +2,22 @@ package frc.trigon.robot.subsystems.collector.simulationcollector;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.math.system.plant.DCMotor;
 
 public class SimulationCollectorIOConstants {
     private static final int MOTOR_ID = 1;
     private static final NeutralMode NEUTRAL_MODE_VALUE = NeutralMode.Coast;
     private static final boolean INVERTED = false;
     private static final int VOLTAGE_COMPENSATION_SATURATION = 12;
+    private static final double
+            nominalVoltageVolts = 1,
+            stallTorqueNewtonMeters = 1,
+            stallCurrentAmps = 1,
+            freeCurrentAmps = 1,
+            freeSpeedRadPerSec = 1;
+    private static final int numMotors = 1;
     static final TalonSRX MOTOR = new TalonSRX(MOTOR_ID);
+    static final DCMotor DC_MOTOR = new DCMotor(nominalVoltageVolts, stallTorqueNewtonMeters, stallCurrentAmps, freeCurrentAmps, freeSpeedRadPerSec, numMotors);
 
     static {
         MOTOR.configFactoryDefault();
