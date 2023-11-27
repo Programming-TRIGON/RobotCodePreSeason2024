c054fdd1bbad0e04789af88802bb9e4b2e2d70c9
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2023-11-27 20:50:17 +0200

    made SimulationArmIOMotors

--- src/main/java/frc/trigon/robot/subsystems/arm/simulationarm/SimulationArmIO.java
+++ src/main/java/frc/trigon/robot/subsystems/arm/simulationarm/SimulationArmIO.java
@@ -1,10 +1,14 @@
 package frc.trigon.robot.subsystems.arm.simulationarm;
 
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.simulation.ElevatorSim;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 import frc.trigon.robot.subsystems.arm.ArmIO;
 import frc.trigon.robot.subsystems.arm.ArmInputsAutoLogged;
 
 public class SimulationArmIO extends ArmIO {
+    private final SingleJointedArmSim angleMotor = SimulationArmIOConstants.ANGLE_MOTOR;
+    private final ElevatorSim elevatorMotor = SimulationArmIOConstants.ELEVATOR_MOTOR;
     @Override
     protected void updateInputs(ArmInputsAutoLogged inputs) {
     }
