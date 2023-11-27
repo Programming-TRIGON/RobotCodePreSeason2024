4e40e1dd908314858cb957e905f95722c0894a7f
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2023-11-27 12:51:38 +0200

    implemented DCMotorSim

--- src/main/java/frc/trigon/robot/subsystems/collector/simulationcollector/SimulationCollectorIO.java
+++ src/main/java/frc/trigon/robot/subsystems/collector/simulationcollector/SimulationCollectorIO.java
@@ -1,43 +1,25 @@
 package frc.trigon.robot.subsystems.collector.simulationcollector;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import edu.wpi.first.math.VecBuilder;
-import edu.wpi.first.math.system.LinearSystem;
-import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
-import edu.wpi.first.wpilibj.simulation.ElevatorSim;
-import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 import frc.trigon.robot.subsystems.collector.CollectorIO;
 import frc.trigon.robot.subsystems.collector.CollectorInputsAutoLogged;
 
 public class SimulationCollectorIO extends CollectorIO {
-    private final TalonSRX motor = SimulationCollectorIOConstants.MOTOR;
-    private final DCMotor dcMotor = SimulationCollectorIOConstants.DC_MOTOR;
-    private final DCMotorSim dcMotorSim =
-            new DCMotorSim(
-                    LinearSystemId.createDCMotorSystem(dcMotor, 1, 1),
-                    dcMotor,
-                    1
-            );
-    public SimulationCollectorIO() {
-        System.out.println("[Init] Creating CollectorIOSim");
-    }
+    private final DCMotorSim dcMotorSim = SimulationCollectorIOConstants.DC_MOTOR_SIM;
 
     @Override
     protected void updateInputs(CollectorInputsAutoLogged inputs) {
-        inputs.motorVoltage = motor.getMotorOutputVoltage();
-        inputs.motorCurrent = motor.getSupplyCurrent();
+        inputs.motorVoltage = dcMotorSim.getOutput().get(0, 0);
+        inputs.motorCurrent = dcMotorSim.getCurrentDrawAmps();
     }
 
     @Override
     protected void setPower(double power) {
-        motor.set(ControlMode.PercentOutput, power);
+        dcMotorSim.setInputVoltage(power);
     }
 
     @Override
     protected void stop() {
-        motor.set(ControlMode.Disabled, 0);
+        dcMotorSim.setInputVoltage(0);
     }
 }
--- src/main/java/frc/trigon/robot/subsystems/collector/simulationcollector/SimulationCollectorIOConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/collector/simulationcollector/SimulationCollectorIOConstants.java
@@ -3,27 +3,34 @@ package frc.trigon.robot.subsystems.collector.simulationcollector;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 
 public class SimulationCollectorIOConstants {
-    private static final int MOTOR_ID = 1;
-    private static final NeutralMode NEUTRAL_MODE_VALUE = NeutralMode.Coast;
-    private static final boolean INVERTED = false;
-    private static final int VOLTAGE_COMPENSATION_SATURATION = 12;
     private static final double
-            nominalVoltageVolts = 1,
-            stallTorqueNewtonMeters = 1,
-            stallCurrentAmps = 1,
-            freeCurrentAmps = 1,
-            freeSpeedRadPerSec = 1;
-    private static final int numMotors = 1;
-    static final TalonSRX MOTOR = new TalonSRX(MOTOR_ID);
-    static final DCMotor DC_MOTOR = new DCMotor(nominalVoltageVolts, stallTorqueNewtonMeters, stallCurrentAmps, freeCurrentAmps, freeSpeedRadPerSec, numMotors);
-
-    static {
-        MOTOR.configFactoryDefault();
-        MOTOR.configVoltageCompSaturation(VOLTAGE_COMPENSATION_SATURATION);
-        MOTOR.enableVoltageCompensation(true);
-        MOTOR.setNeutralMode(NEUTRAL_MODE_VALUE);
-        MOTOR.setInverted(INVERTED);
-    }
+            NOMINAL_VOLTAGE_VOLTS = 1,
+            STALL_TORQUE_NEWTON_METERS = 1,
+            STALL_CURRENT_AMPS = 1,
+            FREE_CURRENT_AMPS = 1,
+            FREE_SPEED_RAD_PER_SEC = 1;
+    private static final int NUM_MOTORS = 1;
+    private static final double
+            JKG_METERS_SQUARED = 1,
+            G = 1,
+            GEARING = 1;
+    static final DCMotor DC_MOTOR =
+            new DCMotor(
+                    NOMINAL_VOLTAGE_VOLTS,
+                    STALL_TORQUE_NEWTON_METERS,
+                    STALL_CURRENT_AMPS,
+                    FREE_CURRENT_AMPS,
+                    FREE_SPEED_RAD_PER_SEC,
+                    NUM_MOTORS
+            );
+    static final DCMotorSim DC_MOTOR_SIM =
+            new DCMotorSim(
+                    LinearSystemId.createDCMotorSystem(DC_MOTOR, JKG_METERS_SQUARED, G),
+                    DC_MOTOR,
+                    GEARING
+            );
 }
