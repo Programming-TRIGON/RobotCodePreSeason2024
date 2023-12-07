0fb2d996c1f67a4f5539b31a02f3f42ff9386625
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2023-12-07 17:16:41 +0200

    attempted to make Turret

--- src/main/java/frc/trigon/robot/subsystems/turret/Turret.java
+++ src/main/java/frc/trigon/robot/subsystems/turret/Turret.java
@@ -1,21 +1,64 @@
 package frc.trigon.robot.subsystems.turret;
 
-import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import java.util.function.Supplier;
 
 public class Turret extends SubsystemBase {
     private final static Turret INSTANCE = new Turret();
     private final TalonFX motor = TurretConstants.MOTOR;
-    private final CANcoder encoder = TurretConstants.ENCODER;
     private final Pose2d pose = TurretConstants.POSE;
-
+    private final VoltageOut voltageRequest = new VoltageOut(0, TurretConstants.FOC_ENABLED, false);
 
     public static Turret getInstance() {
         return INSTANCE;
     }
 
     private Turret() {
+    }
+
+    public Command getSetTurretPositionCommand(Supplier<Double> targetAngle) {
+        return new FunctionalCommand(
+                () -> {
+                },
+                () -> turretCalculate(targetAngle.get()),
+                (interrupted) -> stop(),
+                () -> false,
+                this
+        );
+    }
+
+    private void turretCalculate(double targetAngle) {
+        if (atTarget())
+            return;
+        if (!checkTurretSpinLimit(targetAngle))
+            return;
+        double output = TurretConstants.PID_CONTROLLER.calculate(targetAngle);
+        setMotorVoltage(output);
+    }
+
+    private boolean atTarget() {
+        return getPosition() == TurretConstants.TARGET_POSITION.getAngle().getRotations();
+    }
+
+    private boolean checkTurretSpinLimit(double targetAngle) {
+        return Math.abs(getPosition() - targetAngle) == 200;
+    }
+
+    private double getPosition() {
+        return TurretConstants.STATUS_SIGNAL.refresh().getValue();
+    }
+
+    private void setMotorVoltage(double voltage) {
+        motor.setControl(voltageRequest.withOutput(voltage));
+    }
+
+    private void stop() {
+        motor.stopMotor();
     }
 }
--- src/main/java/frc/trigon/robot/subsystems/turret/TurretConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/turret/TurretConstants.java
@@ -1,5 +1,6 @@
 package frc.trigon.robot.subsystems.turret;
 
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.CANcoder;
@@ -27,16 +28,17 @@ public class TurretConstants {
     static final TalonFX MOTOR = new TalonFX(MOTOR_ID);
     static final CANcoder ENCODER = new CANcoder(ENCODER_ID);
     static final Translation2d TARGET_POSITION = new Translation2d(TRANSLATION_X, TRANSLATION_Y);
+    static final Pose2d POSE = new Pose2d();
     static final double GEAR_RATIO = 100;
     static final double MOMENT_OF_INERTIA = 0.003;
+    static final boolean FOC_ENABLED = true;
+    static final StatusSignal<Double> STATUS_SIGNAL = ENCODER.getPosition();
 
-    static final double
+    private static final double
             P = 1,
             I = 0,
             D = 0;
     static final PIDController PID_CONTROLLER = new PIDController(P, I, D);
-
-    static final Pose2d POSE = new Pose2d();
 
     static {
         configureMotor();
@@ -58,5 +60,8 @@ public class TurretConstants {
         config.MagnetSensor.SensorDirection = ENCODER_DIRECTION;
         config.MagnetSensor.MagnetOffset = ENCODER_OFFSET;
         ENCODER.getConfigurator().apply(config);
+
+        STATUS_SIGNAL.setUpdateFrequency(100);
+        ENCODER.optimizeBusUtilization();
     }
 }
