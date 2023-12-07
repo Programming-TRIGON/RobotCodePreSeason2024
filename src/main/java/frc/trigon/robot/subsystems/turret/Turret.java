ec1ae5aaab2fea2f9525c5b2db3e444273029198
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2023-12-07 20:40:59 +0200

    corrected output calulation

--- src/main/java/frc/trigon/robot/subsystems/turret/Turret.java
+++ src/main/java/frc/trigon/robot/subsystems/turret/Turret.java
@@ -3,6 +3,7 @@ package frc.trigon.robot.subsystems.turret;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -22,34 +23,53 @@ public class Turret extends SubsystemBase {
     private Turret() {
     }
 
-    public Command getSetTurretPositionCommand() {
+    public Command getSetMotorOutputCommand(Pose2d robotPose) {
         return new FunctionalCommand(
                 () -> {
                 },
-                this::turretCalculate,
+                () -> setMotorOutput(robotPose),
                 (interrupted) -> stop(),
                 () -> false,
                 this
         );
     }
 
-    private void turretCalculate() {
-        if (atTarget())
-            return;
-        double xDifference = hubPose.getX() - getPosition();
-        double yDifference = hubPose.getY() - getPosition();
-        double desiredHeading = Math.atan2(xDifference, yDifference);
-        if (checkTurretSpinLimit(desiredHeading))
-            return;
-        setMotorVoltage(desiredHeading);
+    private void setMotorOutput(Pose2d robotPose) {
+        setMotorVoltage(calculateMotorOutput(robotPose));
     }
 
-    private boolean atTarget() {
-        return getPosition() == hubPose.getRotation().getRotations();
+    private double calculateMotorOutput(Pose2d robotPose) {
+        double targetAngle = calculateTargetAngle(robotPose);
+        double targetAngleAfterLimitCheck = limitCheck(targetAngle);
+        double error = calculateError(targetAngleAfterLimitCheck);
+        double output = TurretConstants.PID_CONTROLLER.calculate(error);
+        return output;
     }
 
-    private boolean checkTurretSpinLimit(double angleChange) {
-        return Math.abs(getPosition() - angleChange) == 200;
+    private double calculateTargetAngle(Pose2d robotPose) {
+        double robotX = robotPose.getX();
+        double robotY = robotPose.getY();
+        double robotHeading = robotPose.getRotation().getRadians();
+        double hubVecX = hubPose.getX() - robotX;
+        double hubVecY = hubPose.getY() - robotY;
+        double theta = Math.atan2(hubVecY, hubVecX);
+        double targetAngle = theta - robotHeading;
+        return targetAngle;
+    }
+
+    private double limitCheck(double targetAngle) {
+        if (targetAngle > 200) {
+            targetAngle -= 360;
+        } else if (targetAngle < -200) {
+            targetAngle += 360;
+        }
+        return targetAngle;
+    }
+
+    private double calculateError(double targetAngle) {
+        double currentAngleDegrees = Units.rotationsToDegrees(getPosition());
+        double error = targetAngle - currentAngleDegrees;
+        return error;
     }
 
     private double getPosition() {
