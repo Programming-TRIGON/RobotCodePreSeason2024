641404446f08afb26778edc31dfb427d244edc43
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2023-12-13 13:00:59 +0200

    added pid usage

--- src/main/java/frc/trigon/robot/subsystems/turret/kablamaturret/KablamaTurretIO.java
+++ src/main/java/frc/trigon/robot/subsystems/turret/kablamaturret/KablamaTurretIO.java
@@ -1,6 +1,6 @@
 package frc.trigon.robot.subsystems.turret.kablamaturret;
 
-import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.math.util.Units;
 import frc.trigon.robot.subsystems.turret.TurretIO;
@@ -8,7 +8,7 @@ import frc.trigon.robot.subsystems.turret.TurretInputsAutoLogged;
 
 public class KablamaTurretIO extends TurretIO {
     private final TalonFX motor = KablamaTurretConstants.MOTOR;
-    private final VoltageOut voltageRequest = new VoltageOut(0, KablamaTurretConstants.FOC_ENABLED, false);
+    private final PositionVoltage voltageRequest = new PositionVoltage(0);
 
     @Override
     protected void updateInputs(TurretInputsAutoLogged inputs) {
@@ -17,7 +17,7 @@ public class KablamaTurretIO extends TurretIO {
 
     @Override
     protected void calculateMotorVoltage(double voltage) {
-        setMotorVoltage(voltage);
+        motor.setControl(voltageRequest.withPosition(voltage));
     }
 
     @Override
@@ -27,9 +27,5 @@ public class KablamaTurretIO extends TurretIO {
 
     private double getPositionDegrees() {
         return Units.rotationsToDegrees(KablamaTurretConstants.TURRET_POSITION_SIGNAL.refresh().getValue());
-    }
-
-    private void setMotorVoltage(double voltage) {
-        motor.setControl(voltageRequest.withOutput(voltage));
     }
 }
