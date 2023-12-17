b3d2c41d05a12862007426c8436c8ded4a22b617
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2023-12-17 16:31:08 +0200

    added simulation

--- src/main/java/frc/trigon/robot/subsystems/turret/simulationturret/SimulationTurretConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/turret/simulationturret/SimulationTurretConstants.java
@@ -1,7 +1,26 @@
 package frc.trigon.robot.subsystems.turret.simulationturret;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import frc.trigon.robot.subsystems.turret.TurretIO;
 
 public class SimulationTurretConstants extends TurretIO {
-    
+    private static final int MOTOR_AMOUNT = 1;
+    private static final DCMotor MOTOR_GEARBOX = DCMotor.getFalcon500Foc(MOTOR_AMOUNT);
+    private static final double MOMENT_OF_INERTIA = 0.003;
+    private static final double GEAR_RATIO = 12.8;
+
+    static final DCMotorSim MOTOR = new DCMotorSim(
+            MOTOR_GEARBOX,
+            GEAR_RATIO,
+            MOMENT_OF_INERTIA
+    );
+
+    private static final double
+            P = 1,
+            I = 0,
+            D = 0;
+
+    static final PIDController PID_CONTROLLER = new PIDController(P, I, D);
 }
--- src/main/java/frc/trigon/robot/subsystems/turret/simulationturret/SimulationTurretIO.java
+++ src/main/java/frc/trigon/robot/subsystems/turret/simulationturret/SimulationTurretIO.java
@@ -1,6 +1,25 @@
 package frc.trigon.robot.subsystems.turret.simulationturret;
 
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import frc.trigon.robot.subsystems.turret.TurretIO;
+import frc.trigon.robot.subsystems.turret.TurretInputsAutoLogged;
 
 public class SimulationTurretIO extends TurretIO {
+    private final DCMotorSim motor = SimulationTurretConstants.MOTOR;
+    
+    @Override
+    protected void updateInputs(TurretInputsAutoLogged inputs) {
+        inputs.motorPositionDegrees = Units.radiansToDegrees(motor.getAngularPositionRad());
+    }
+
+    @Override
+    protected void calculateMotorVoltage(double voltage) {
+        motor.setInputVoltage(SimulationTurretConstants.PID_CONTROLLER.calculate(voltage));
+    }
+
+    @Override
+    protected void stop() {
+        motor.setInputVoltage(0);
+    }
 }
