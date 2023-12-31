ead0c5376a1bd0121ab7626e409b06e264bec5c8
Strflightmight09 <ezragryn57@gmail.com>
2023-12-31 22:32:59 +0200

    Tried some stuff for the wrapper

--- src/main/java/frc/trigon/robot/subsystems/turret/simulationturret/SimulationTurretConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/turret/simulationturret/SimulationTurretConstants.java
@@ -1,38 +1,40 @@
 package frc.trigon.robot.subsystems.turret.simulationturret;
 
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import frc.trigon.robot.simulationwrapper.DCMotorSimulation;
 
 public class SimulationTurretConstants {
-    static final double VOLTAGE_COMPENSATION_SATURATION = 12;
+    public static final double VOLTAGE_COMPENSATION_SATURATION = 12;
 
     private static final int MOTOR_AMOUNT = 1;
     private static final DCMotor MOTOR_GEARBOX = DCMotor.getFalcon500Foc(MOTOR_AMOUNT);
     private static final double GEAR_RATIO = 100;
     private static final double MOMENT_OF_INERTIA = 0.003;
-    static final DCMotorSim MOTOR = new DCMotorSim(
+    public static final DCMotorSim MOTOR = new DCMotorSim(
             MOTOR_GEARBOX,
             GEAR_RATIO,
             MOMENT_OF_INERTIA
     );
-
     private static final double
             MAX_MOTOR_VELOCITY = 5,
             MAX_MOTOR_ACCELERATION = 3;
-    private static final TrapezoidProfile.Constraints CONSTRAINTS = new TrapezoidProfile.Constraints(MAX_MOTOR_VELOCITY, MAX_MOTOR_ACCELERATION);
-
     private static final double
             P = 0,
             I = 0,
             D = 0;
-    static final ProfiledPIDController PROFILED_PID_CONTROLLER = new ProfiledPIDController(P, I, D, CONSTRAINTS);
-
     private static final double
             KS = 0,
             KV = 0,
             KA = 0;
-    static final SimpleMotorFeedforward FEEDFORWARD = new SimpleMotorFeedforward(KS, KV, KA);
+
+    public static DCMotorSimulation MOTOR_SIMULATION = new DCMotorSimulation();
+
+    static {
+        MOTOR_SIMULATION.motorSim = MOTOR;
+        MOTOR_SIMULATION.constraints = new TrapezoidProfile.Constraints(MAX_MOTOR_VELOCITY, MAX_MOTOR_ACCELERATION);
+        MOTOR_SIMULATION.setPID(P, I, D);
+        MOTOR_SIMULATION.setFeedforward(KS, KV, KA);
+    }
 }
--- src/main/java/frc/trigon/robot/subsystems/turret/simulationturret/SimulationTurretIO.java
+++ src/main/java/frc/trigon/robot/subsystems/turret/simulationturret/SimulationTurretIO.java
@@ -1,22 +1,20 @@
 package frc.trigon.robot.subsystems.turret.simulationturret;
 
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import frc.trigon.robot.constants.RobotConstants;
+import frc.trigon.robot.simulationwrapper.DCMotorSimulation;
 import frc.trigon.robot.subsystems.turret.TurretIO;
 import frc.trigon.robot.subsystems.turret.TurretInputsAutoLogged;
-import frc.trigon.robot.utilities.Conversions;
 
 public class SimulationTurretIO extends TurretIO {
-    private final DCMotorSim motor = SimulationTurretConstants.MOTOR;
+    private final DCMotorSimulation DCMotorSimulation = SimulationTurretConstants.MOTOR_SIMULATION;
 
     private double motorVoltage;
 
     @Override
     protected void updateInputs(TurretInputsAutoLogged inputs) {
-        motor.update(RobotConstants.PERIODIC_TIME_SECONDS);
+        DCMotorSimulation.motorSim.update(RobotConstants.PERIODIC_TIME_SECONDS);
 
         inputs.motorVoltage = motorVoltage;
         inputs.motorAngleDegrees = getMotorAngleDegrees();
@@ -25,35 +23,22 @@ public class SimulationTurretIO extends TurretIO {
 
     @Override
     protected void setTargetAngle(Rotation2d targetAngle) {
-        setMotorVoltageFromPower(calculateMotorVoltage(targetAngle));
+        double voltage = SimulationTurretConstants.MOTOR_SIMULATION.calculateTargetMotorVoltage(targetAngle);
+        motorVoltage = voltage;
+        DCMotorSimulation.setMotorVoltageFromPower(voltage);
     }
 
     @Override
     protected void stop() {
-        setMotorVoltageFromPower(0);
-    }
-
-    private double calculateMotorVoltage(Rotation2d targetAngle) {
-        double pidOutput = SimulationTurretConstants.PROFILED_PID_CONTROLLER.calculate(targetAngle.getRotations());
-        SimulationTurretConstants.PROFILED_PID_CONTROLLER.setGoal(0);
-        double feedforward = SimulationTurretConstants.FEEDFORWARD.calculate(SimulationTurretConstants.PROFILED_PID_CONTROLLER.getGoal().velocity);
-        return pidOutput + feedforward;
-    }
-
-    private void setMotorVoltageFromPower(double power) {
-        motorVoltage = MathUtil.clamp(
-                Conversions.compensatedPowerToVoltage(power, SimulationTurretConstants.VOLTAGE_COMPENSATION_SATURATION),
-                -SimulationTurretConstants.VOLTAGE_COMPENSATION_SATURATION,
-                SimulationTurretConstants.VOLTAGE_COMPENSATION_SATURATION
-        );
-        motor.setInputVoltage(motorVoltage);
+        motorVoltage = 0;
+        DCMotorSimulation.setMotorVoltageFromPower(0);
     }
 
     private double getMotorAngleDegrees() {
-        return Units.radiansToDegrees(motor.getAngularPositionRad());
+        return Units.radiansToDegrees(DCMotorSimulation.motorSim.getAngularPositionRad());
     }
 
     private double getMotorVelocityDegreesPerSecond() {
-        return Units.radiansToDegrees(motor.getAngularVelocityRadPerSec());
+        return Units.radiansToDegrees(DCMotorSimulation.motorSim.getAngularVelocityRadPerSec());
     }
 }
